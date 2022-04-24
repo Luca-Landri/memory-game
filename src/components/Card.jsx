0d47f64fad78135979b76bde8e29b04c7ad550d9
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-04-25 00:49:49 +0200

    Some changes to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
-
+import "./Card.css";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const CardContainer = styled.div`
+const CardContainer = styled(motion.div)`
+    position: relative;
     display: flex;
     flex-basis: 15%;
     flex-direction: column;
@@ -9,34 +11,30 @@ const CardContainer = styled.div`
     align-items: center;
     margin: 10px;
     border-radius: 5px;
-    transition: all 0.3s ease-in-out;
 
-`;
-
-const FrontImg = styled.img`
-    width: 200px;
-`;
-
-const BackImg = styled.img`
-    width: 200px;
-`;
-
-const Card = ({src, handleChoice, flipped}) => {
-
-    const handleClick = () => {
-        handleChoice(src);
+    img {
+        display: block;
+        border-radius: 15px;
+        border: 4px solid #fff;
     }
 
+    @media (max-width: 600px) {
+        flex-basis: 10%;
+    }
+`;
+
+
+const Card = ({card, handleChoice, flipped}) => {
+
+    const handleClick = () => {
+        handleChoice(card);
+    }
 
     return(
-        <CardContainer>
-            <div>
-                {
-                flipped ? 
-                <FrontImg src={src} alt='card front'/>                
-                : 
-                <BackImg onClick={handleClick} src="https://i.imgur.com/0V81Ft7.png" alt="card cover"/>
-                }
+        <CardContainer className="card" whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+            <div className={flipped ? "flipped" : ""}>
+                <img className="front" src={card.src} alt='card front'/>                
+                <img className="back" onClick={handleClick} src="https://i.imgur.com/x7L9qpC.png" alt="card cover"/>
             </div>
         </CardContainer>
     );
