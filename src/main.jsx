f7631af7a1596167be877b1c8d6a4b57ffc1f69d
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-04-25 17:05:23 +0200

    Created a router in the main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Game from './Game';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="/game" element={<Game />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
